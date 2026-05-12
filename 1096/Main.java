6c8f4fcb9f4a4ad0962c4c6e7da1d22d89044fdf
PushkarShinde <pushkarshinde249@gmail.com>
2026-05-12 12:44:07 +0530

    Automated daily sync - 2026-05-12 12:44

---
+++ 1096/Main.java
@@ -0,0 +1,113 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+  static FastReader in = new FastReader();
+  static PrintWriter out = new PrintWriter(System.out);
+
+  static int[] bit;
+
+  static void update(int i){
+    for(i++;i<bit.length;i+=(i&(-i))) bit[i]++;
+  }
+  public static void main(String[] args) throws Exception {
+    int t=in.nextInt(); 
+    StringBuilder res=new StringBuilder();
+    while (t-- > 0) {
+      solve(res);
+    }
+    System.out.println(res);
+    out.flush();
+  }
+
+  static void solve(StringBuilder res){
+    int n=in.nextInt();
+    long[] a=new long[n];
+    for(int i=0; i<n; i++) a[i]=in.nextLong();
+    int l=0;
+    long conseq=0;
+    long ans=0;
+    for(int i=1;i<n-1;i++){
+      long pre=a[i-1];
+      long cur=a[i];
+      long nxt=a[i+1];
+
+      if(cur>=(pre+nxt)){
+        conseq=i-l+1;
+        l=i+1;
+        ans+=conseq*conseq;
+        break;
+      }
+    }
+    res.append(ans).append("\n");
+  }
+
+    // Fast I/O template
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    // GCD
+    static long gcd(long a, long b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    // LCM
+    static long lcm(long a, long b) {
+        return a/gcd(a,b)*b;
+    }
+
+    // Sieve of Eratosthenes
+    static boolean[] sieve(int n) {
+        boolean[] isPrime = new boolean[n+1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+        for (int i = 2; i*i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i*i; j <= n; j += i) isPrime[j] = false;
+            }
+        }
+        return isPrime;
+    }
+
+    // Binary Search Template
+    static int binarySearch(int[] arr, int target) {
+        int l = 0, r = arr.length - 1;
+        while (l <= r) {
+            int m = l+(r-l)/ 2;
+            if (arr[m] == target) return m;
+            else if (arr[m] < target) l = m + 1;
+            else r = m - 1;
+        }
+        return -1;
+    }
+
+}
